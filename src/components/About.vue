8e359d68ca66e21c7bbd005a9765a79b0f917436
Andrei Dogaru <andreidogaru99@gmail.com>
2019-08-07 23:42:18 +0300

    Project complete

---
+++ src/components/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>Hello friend, this is an about page</h1>
+    <p>This single page app was made in order to learn Vue.js with routing.</p>
+    <p>It was made using this <a :href="resource" target="_blank">tutorial</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data(){
+    return{
+      resource:"https://github.com/designcourse/vue2-tutorial"
+    }
+  },
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  scoped>
+  
+</style>
